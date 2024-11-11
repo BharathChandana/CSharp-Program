670d8a37a96cc74bbf48fe1cb18b735422b8ac26
HP <HP@DESKTOP-4G77UKH>
2024-11-11 10:36:49 +0530

    Add project files.

---
+++ Angle blw hr and min hand/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Angle_blw_hr_and_min_hand
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter time in hour ");
+            int hr=int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter time in minutes ");
+            int min=int.Parse(Console.ReadLine());
+
+            double hrdeg = ((hr %12)*30) + (min * 30 / 60);
+            double mindeg = (min * 6);
+
+            double dif=Math.Abs(hrdeg - mindeg);
+            if(dif>180)
+            {
+                dif=360-dif;
+            }
+            Console.WriteLine($"Angle b/w min and hour hand is {dif}");
+        }
+    }
+}
